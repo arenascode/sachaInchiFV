1ced4c7434359e1b42d6f4ee70e754c1a1d55678
Miguel Arenas <migue.arenas91@gmail.com>
2024-12-12 17:30:48 -0500

    sticky nav logic done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
 
 console.log('hello world!!');
+ const nav = document.querySelector(".nav");
+ const topOfNav = nav.offsetTop;
 
+ function fixNav() {
+   const offsetTop = window.scrollY;
+   console.log({offsetTop});
+   console.log({topOfNav});
+   
+   if (offsetTop >= topOfNav) {
+     document.body.classList.add("fixed-nav");
+     document.body.style.paddingTop = nav.offsetHeight + "px";
+   } else {
+     document.body.style.paddingTop = 0;
+     document.body.classList.remove("fixed-nav");
+   }
+ }
+
+ const moreinfoHeroBtn = document.getElementById('moreInfoHero'
+ )
+
+ const targetSection = document.getElementById('whatIsSachaInchi')
+
+moreinfoHeroBtn.addEventListener('click', () => {
+  targetSection.scrollIntoView({ behavior: "smooth", block:"start"})
+ })
+ window.addEventListener("scroll", fixNav);
